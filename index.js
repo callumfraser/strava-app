13a5e276c2c4cf5d29937a4e5c4d497ab232eb53
Callum Fraser <callumfraser@Callums-MacBook-Pro.local>
2018-07-28 19:04:29 +0200

    fixed ajax call

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var loadAjaxPost = function(method, url, data, cb) {
         if (xhr.readyState === 4) {
             if (xhr.status === 200) {
               console.log(xhr);
-              cb();
+              cb(data);
             } else {
                 console.log("error" + xhr.status)
             }
