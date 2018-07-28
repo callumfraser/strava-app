29f71bce2a5c98ed0c762bda831c5372c0fff93c
Callum Fraser <callumfraser@Callums-MacBook-Pro.local>
2018-07-28 15:15:22 +0200

    testing initial oauth flow

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 var express = require('express');
 var app = express();
+var express_handlebars = require('express-handlebars');
 var port = process.env.PORT || 4000;
-
-function testHeroku(name){
-  return "Hi there, " + name;
-}
+var $ = require('jquery');
+app.engine('handlebars', express_handlebars({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
 
 app.get('/', function(req,res){
-  res.send(testHeroku("Callum"));
+  res.render('landing', {
+      message: "Welcome to my Strava Visualisation App! Please click below to login via Strava"
+  });
 });
+
+app.post('/', function(req,res){
+  if (login){
+    res.redirect("https://www.strava.com/oauth/authorize?client_id=27332&redirect_uri=https://gentle-falls-53808.herokuapp.com/user/&response_type=code");
+  }
+});
+
+app.get('/user', function(req,res){
+  res.send("Hello, Callum. Are you accessing from " + $client->getResponse()->headers->get('location') + "?";
+})
 
 app.listen(port, function() {
     console.log("App listening on port")
