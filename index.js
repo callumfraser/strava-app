4604cc40893af202fdddfd57a3dbf6da6e58bc5b
Callum Fraser <callumfraser@Callums-MacBook-Pro.local>
2018-07-31 20:23:57 +0200

    call athlete id in sortactivities function

--- index.js
+++ index.js
@@ -48,106 +48,6 @@ Handlebars.registerHelper('cleanDate', function(date) {
   return actDate.toLocaleDateString("en-US", options);
 });
 
-
-// function countWeeks(startTime){
-//   var now = new Date(moment().format());
-//   var startDate = new Date(startTime);
-//   var timeDiff = Math.abs(now.getTime() - startDate.getTime());
-//   var amountOfDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
-//   // console.log("AMOUNT OF DAYS: " + amountOfDays);
-//   var weeks = Math.ceil(amountOfDays/7);
-//   return weeks;
-// };
-
-// function calculateActivities(activArr,weeks){
-//   var noOfActs = activArr.length;
-//   var avPerWeek = noOfActs/weeks;
-//   var totalDistance = 0;
-//   var totalAvSpeed = 0;
-//   var fastestAvSpeed = 0;
-//   var longestActDistance = 0;
-//   var longestActDuration = 0;
-//   for (var i=0;i<activArr.length;i++){
-//     if (activArr[i].average_speed > fastestAvSpeed){
-//       fastestAvSpeed = activArr[i].average_speed;
-//     };
-//     if (activArr[i].distance > longestActDistance){
-//       longestActDistance = activArr[i].distance;
-//     }
-//     if (activArr[i].elapsed_time > longestActDuration){
-//       longestActDuration = activArr[i].elapsed_time;
-//     }
-//     totalDistance += activArr[i].distance;
-//     totalAvSpeed += activArr[i].average_speed;
-//   };
-//   var avDistancePerAct = (totalDistance/noOfActs);
-//   var avAvSpeedPerAct = (totalAvSpeed/noOfActs);
-//   var summary = {
-//     avPerWeek: avPerWeek,
-//     avDistancePerAct: avDistancePerAct,
-//     avAvSpeedPerAct: avAvSpeedPerAct,
-//     fastestAvSpeed: fastestAvSpeed,
-//     longestActDistance:  longestActDistance,
-//     longestActDuration: longestActDuration
-//   }
-//   return summary;
-// };
-//
-//
-//
-// function analyseActivities(runs,rides,weeks){
-//   var now = moment().format();
-//   var runSummary = {};
-//   var rideSummary = {};
-//   var emptySet = {
-//     avPerWeek: 0,
-//     avDistancePerAct: 0,
-//     avAvSpeedPerAct: 0,
-//     fastestAvSpeed: 0,
-//     longestActDistance:  0,
-//     longestActDuration: 0
-//   };
-//   if (runs.length > 0){
-//     runSummary = calculateActivities(runs,weeks);
-//   } else {
-//     runSummary = emptySet;
-//   };
-//   if (rides.length > 0){
-//     rideSummary = calculateActivities(rides,weeks);
-//   } else {
-//     rideSummary = emptySet;
-//   };
-//   var completeSummary = {
-//     date: now,
-//     id: athleteId,
-//     rides: rideSummary,
-//     runs: runSummary
-//   };
-//   return completeSummary;
-// };
-//
-// function sortActivities(response,startTime){
-//   // console.log("SORT ACTIVITIES -> " + response)
-//   var startDateVal = new Date(startTime).getTime();
-//   var weeks = countWeeks(startTime);
-//   var runs = [];
-//   var rides = [];
-//   for (var i=0;i<response.length;i++){
-//     var actDate = new Date(response[i].start_date).getTime();
-//     if (actDate > startDateVal){
-//       if (response[i].type == "Run"){
-//         runs.push(response[i]);
-//       } else if (response[i].type == "Ride"){
-//         rides.push(response[i]);
-//       };
-//     };
-//   };
-//   return analyseActivities(runs,rides,weeks);
-// };
-
-
-
-
 const mongoURL = process.env.MONGO_DB_URL || "mongodb://localhost/StravaAPIs"
 mongoose.connect(mongoURL);
 
@@ -210,11 +110,14 @@ app.get('/welcome', function(req,res){
     var query = {
       'id': athleteId
     };
+    console.log("HOW MANY ARE THERE!!")
+
     var previousSummaries = searchID(summaryDB,query);
     newInput = sortActivities(payload,startReqDate,athleteId);
 
 
     function renderResults(){
+      console.log(previousSummaries.length);
       console.log("RENDER THESE -> ")
       // console.log(previousSummaries);
       res.render('user', {
